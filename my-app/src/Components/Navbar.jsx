8ad4e3a9a9c3c4976d29c5e347c79fb3ce27147f
Suraj pawar <101104316+Suraj2320@users.noreply.github.co>
2022-09-28 14:48:32 +0530

    navbar

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  static propTypes = {second: third}
+
+  render() {
+    return (
+      <div>Navbar</div>
+    )
+  }
+}
